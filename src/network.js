1d3d260c327b4cbb63b40cfd2cf967bcff59d18b
netanelben <mtr574@gmail.com>
2021-03-13 14:55:27 +0200

    -

--- src/network.js
+++ src/network.js
@@ -1,5 +1,7 @@
 import io from 'socket.io-client';
-const socket = io('https://digipet-blockchain.herokuapp.com:4000');
+
+const socket_url = ':4000';
+const socket = io(socket_url);
 
 export const subscribeTo = (type, callback) => {
   socket.on('channel', function(data){
